6c359eef5f734b9e965eeafc6e7968717f98b16b
Yupeng Wang <yupengori@gmail.com>
2024-11-15 16:41:55 +0100

    watch list

--- config/routes.rb
+++ config/routes.rb
@@ -8,7 +8,7 @@ Rails.application.routes.draw do
   # Defines the root path route ("/")
   # root "posts#index"
   #
- root "lists#index"
+  root to: "lists#index"
 
   resources :lists, except: :index do
     resources :bookmarks, only: [:new, :create]
